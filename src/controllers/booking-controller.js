2bd88866fd391f534ea323a6e7951f5eece79499
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-07-28 13:48:55 +0530

    issues: unable to fatch data from flight service

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,28 @@
+const{StatusCodes} = require('http-status-codes');
+const { BookingService } = require('../services');
+// const { response } = require('express');
+const{SuccessResponce, ErrorResponce} = require('../utils/common');
+// const { data } = require('../utils/common/error-response');
+
+async function createBooking(req, res){
+    try {
+        const response = await BookingService.createBooking({
+            fightId: req.body.fightId,
+
+        });
+        SuccessResponce.data = response;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponce);
+
+    } catch (error) {
+        ErrorResponce.error = error;
+        return res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .json(ErrorResponce);
+    }
+}
+
 module.exports = {
-    
+    createBooking
 }
--- src/services/Booking-service.js
+++ src/services/Booking-service.js
@@ -1,3 +1,22 @@
+const axios = require('axios');
+
+const { BookingRepository } = require('../repositories');
+const { ServerConfig } = require('../config')
+const db = require('../models');
+
+async function createBooking(data) {
+    try {
+       const result = db.sequelize.transaction(async function bookingImp(t){
+           const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+           console.log(flight.data);
+           return true;
+       }); 
+    } catch (error) {
+        throw error;
+    }
+}
+
+
 module.exports = {
-    
+    createBooking
 }
